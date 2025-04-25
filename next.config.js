58be7928945d71f907434e228eed5c52d2147529
Ishan Pathak <ishan.pathak2711@gmail.com>
2025-04-25 03:40:24 -0400

    added new b

--- next.config.js
+++ next.config.js
@@ -1,19 +1,22 @@
 const isDev = process.env.NODE_ENV === 'development'
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  output: 'standalone',
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-  ...(isDev && {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:8000/:path*',
-        },
-      ];
-    },
-  }),
-};
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: isDev
+          ? 'http://localhost:8000/:path*'
+          : 'https://unifieddata-api-552541459765.us-central1.run.app/api/:path*',
+      },
+    ];
+  },
+}
+
+module.exports = nextConfig
